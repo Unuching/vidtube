abda83cae1289196e720c7e3c395a3ab650604d1
Unuching Marma <unuching.marma2023@gmail.com>
2025-06-18 12:59:24 +0600

    add new file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
 dotenv.config({
   path: "./.env",
 });
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connectionerror", err);
+  });
