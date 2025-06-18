7c08d403a3bf37308a1d70c835d8bb545b30a5e5
Unuching Marma <unuching.marma2023@gmail.com>
2025-06-18 11:58:00 +0600

    add new files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello from Unuching backend ");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+dotenv.config({
+  path: "./.env",
+});
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
